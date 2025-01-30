5f7f3d43123a18cd3484e9b73dc770b853847464
TheFoxySin <35858301+SrKenny@users.noreply.github.com>
2025-01-29 22:54:24 -0300

    Update api.jsx
    removing the API key leak

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+// get your api in https://www.themoviedb.org/settings/api
 //Base da URL: https://api.themoviedb.org/3/
-//URL da API: /movie/11?api_key=259200299a990976070675ccb9fd1ca4
+//URL da API: /movie/11?api_key=
 
 const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3/'
